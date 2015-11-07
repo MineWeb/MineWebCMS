ea6a6a9f4d6b7bcb0e42d177dbf98f128288c91c
Valentin <contact@eywek.fr>
2015-11-07 23:18:42 +0100

    fix

--- app/Controller/Component/StatisticsComponent.php
+++ app/Controller/Component/StatisticsComponent.php
@@ -55,7 +55,7 @@ class StatisticsComponent extends Object {
         } else {
           $referer = 'null';
         }
-        $language = $_SERVER['HTTP_ACCEPT_LANGUAGE'];
+        $language = (isset($_SERVER['HTTP_ACCEPT_LANGUAGE'])) ? $_SERVER['HTTP_ACCEPT_LANGUAGE'] : 'null';
         $language = $language{0}.$language{1};
         $this->Visit->read(null, null);
         $this->Visit->set(array('ip' => $_SERVER["REMOTE_ADDR"], 'referer' => $referer, 'lang' => $language, 'navigator' => $_SERVER['HTTP_USER_AGENT'], 'page' => "http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']));
