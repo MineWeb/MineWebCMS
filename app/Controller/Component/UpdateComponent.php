a602a7ceb1b849bc1970134b0dcbebce30a55010
Valentin <contact@eywek.fr>
2015-12-20 22:59:21 +0100

    Modification du component Update (get des nouvelles versions)

--- app/Controller/Component/UpdateComponent.php
+++ app/Controller/Component/UpdateComponent.php
@@ -5,17 +5,22 @@
 class UpdateComponent extends Object {
 
 	public $components = array('Session', 'Configuration', 'Lang');
-  
+
+	private $update = array('status' => false, 'version' => NULL, 'type' => NULL, 'visible' => false);
+	private $cmsVersion = NULL;
+
 	function shutdown(&$controller) {}
 	function beforeRender(&$controller) {}
 	function beforeRedirect() {}
 	function initialize(&$controller) {
 		$controller->set('Update', new UpdateComponent());
 
+		$this->check();
+
 		if(!file_exists(ROOT.'/config/update') OR strtotime('+5 hours', filemtime(ROOT.'/config/update')) < time()) {
-			if($this->available() != false) {
-				if($this->get_type() == "forced") {
-					$this->update($this->get_version());
+			if($this->update->status) {
+				if($this->update->type == "forced") {
+					$this->update($this->update->version);
 				}
 			}
 		}
@@ -25,17 +30,12 @@ class UpdateComponent extends Object {
 	public function available() {
 		if(!file_exists(ROOT.'/config/update') OR strtotime('+5 hours', filemtime(ROOT.'/config/update')) < time()) {
 			App::import('Component', 'LangComponent');
-	    	$this->Lang = new LangComponent();
-	    	App::import('Component', 'ConfigurationComponent');
-	    	$this->Configuration = new ConfigurationComponent();
-			$last_version = $this->get_version();
-			$cms_version = $this->Configuration->get('version');
-			if($cms_version != $last_version) {
-				$type = $this->get_type();
-				$visible = $this->get_visible();
-				if($visible AND $type == "choice") { // choice -> l'utilisateur choisis ou pas, forced -> la màj est faite automatiquement
-					file_put_contents(ROOT.'/config/update', '<div class="alert alert-info">'.$this->Lang->get('UPDATE_AVAILABLE').' '.$this->Lang->get('YOUR_VERSION').' : '.$cms_version.', '.$this->Lang->get('UPDATE_VERSION').' : '.$last_version.' <a href="'.Router::url(array('controller' => 'update', 'action' => 'index', 'admin' => true)).'" style="margin-top: -6px;" class="btn btn-info pull-right">'.$this->Lang->get('UPDATE').'</a></div>');
-					return '<div class="alert alert-info">'.$this->Lang->get('UPDATE_AVAILABLE').' '.$this->Lang->get('YOUR_VERSION').' : '.$cms_version.', '.$this->Lang->get('UPDATE_VERSION').' : '.$last_version.' <a href="'.Router::url(array('controller' => 'update', 'action' => 'index', 'admin' => true)).'" style="margin-top: -6px;" class="btn btn-info pull-right">'.$this->Lang->get('UPDATE').'</a></div>';
+	    $this->Lang = new LangComponent();
+
+			if($this->update->status) {
+				if($this->update->visible AND $this->update->type == "choice") { // choice -> l'utilisateur choisis ou pas, forced -> la màj est faite automatiquement
+					file_put_contents(ROOT.'/config/update', '<div class="alert alert-info">'.$this->Lang->get('UPDATE_AVAILABLE').' '.$this->Lang->get('YOUR_VERSION').' : '.$this->cmsVersion.', '.$this->Lang->get('UPDATE_VERSION').' : '.$this->update->version.' <a href="'.Router::url(array('controller' => 'update', 'action' => 'index', 'admin' => true)).'" style="margin-top: -6px;" class="btn btn-info pull-right">'.$this->Lang->get('UPDATE').'</a></div>');
+					return '<div class="alert alert-info">'.$this->Lang->get('UPDATE_AVAILABLE').' '.$this->Lang->get('YOUR_VERSION').' : '.$this->cmsVersion.', '.$this->Lang->get('UPDATE_VERSION').' : '.$this->update->version.' <a href="'.Router::url(array('controller' => 'update', 'action' => 'index', 'admin' => true)).'" style="margin-top: -6px;" class="btn btn-info pull-right">'.$this->Lang->get('UPDATE').'</a></div>';
 				} else {
 					file_put_contents(ROOT.'/config/update', 1);
 					return false;
@@ -52,11 +52,22 @@ class UpdateComponent extends Object {
 		}
 	}
 
-	public function get_version() {
-		// va check la dernière version sur mineweb.org
-		$get = @file_get_contents('http://mineweb.org/api/v1/get_update');
-		$get = json_decode($get, true);
-		return $get['last_version'];
+	private function check() {
+		$lastVersion = @file_get_contents('http://mineweb.org/api/v1/get_update');
+		$lastVersion = json_decode($lastVersion, true);
+
+		App::import('Component', 'ConfigurationComponent');
+		$this->Configuration = new ConfigurationComponent();
+		$cmsVersion = $this->Configuration->get('version');
+
+		$this->cmsVersion = $cmsVersion;
+
+		if(version_compare($lastVersion['last_version'], $cmsVersion, '<')) {
+			$this->update['status'] = true;
+			$this->update['version'] = $lastVersion['last_version'];
+			$this->update['type'] = $lastVersion['type'];
+			$this->update['visible'] = $lastVersion['visible'];
+		}
 	}
 
 	public function get_update_files($version) {
@@ -105,25 +116,11 @@ WCqkx22behAGZq6rhwIDAQAB
 
 		if (!is_dir(ROOT.'/temp/')) mkdir(ROOT.'/temp/');
 		$write = fopen(ROOT.'/temp/'.$version.'.zip', 'w+');
-		if(!fwrite($write, $zip)) { 
+		if(!fwrite($write, $zip)) {
 			return false;
 		}
 		fclose($write);
 		return true;
-	}
-
-	public function get_type() {
-		// récupére le type de la dernière màj (forcé ou pas)
-		$get = @file_get_contents('http://mineweb.org/api/v1/get_update');
-		$get = json_decode($get, true);
-		return $get['type'];
-	}
-
-	public function get_visible() {
-		// récupére si la màj est signaler à l'utilisateur ou pas
-		$get = @file_get_contents('http://mineweb.org/api/v1/get_update');
-		$get = json_decode($get, true);
-		return $get['visible'];
 	}
 
 	public function stop_update() {
@@ -165,7 +162,7 @@ WCqkx22behAGZq6rhwIDAQAB
 						$contents = str_replace("\r\n", "\n", $contents);
 					}
 					$updateThis = '';
-					
+
 					if($thisFileName == 'modify.php') {
 						$upgradeExec = fopen('modify.php','w');
 						fwrite($upgradeExec, $contents);
@@ -219,7 +216,7 @@ WCqkx22behAGZq6rhwIDAQAB
 
 		if (!is_dir(ROOT.'/temp/')) mkdir(ROOT.'/temp/');
 		$write = fopen(ROOT.'/temp/'.$plugin_name.'-'.$plugin_id.'.zip', 'w+');
-		if(!fwrite($write, $zip)) { 
+		if(!fwrite($write, $zip)) {
 			return false;
 		}
 		fclose($write);
@@ -249,7 +246,7 @@ WCqkx22behAGZq6rhwIDAQAB
 					$contents = str_replace("\r\n", "\n", $contents);
 				}
 				$updateThis = '';
-				
+
 				if($thisFileName == $plugin_name.'/'.$plugin_name.'_update.php') {
 					$upgradeExec = fopen(ROOT.'/temp/'.$plugin_name.'_update.php','w');
 					fwrite($upgradeExec, $contents);
@@ -282,7 +279,7 @@ WCqkx22behAGZq6rhwIDAQAB
 	}
 
 	public function set_log($action, $state = "success", $args, $rand) {
-		// set les logs de la mise à jour 
+		// set les logs de la mise à jour
 		$filename = ROOT.'/app/tmp/logs/update/'.$this->get_version().'-'.$rand.'.log';
 		if(!file_exists($filename)) {
 			if(!is_dir(ROOT.'/app/tmp/logs/update')) {
