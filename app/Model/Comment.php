3c183ae24a23f5c2d11509314b20fb4ce8ae8a29
Valentin <contact@eywek.fr>
2016-03-30 16:17:08 +0200

    Fix cache news

--- app/Model/Comment.php
+++ app/Model/Comment.php
@@ -13,4 +13,13 @@ class Comment extends AppModel {
         'foreignKey' => 'news_id'
     )
   );
+
+	public function afterSave($created, $options = array()) {
+		Cache::delete('news', 'data');
+	}
+
+	public function afterDelete($cascade = true) {
+		Cache::delete('news', 'data');
+	}
+	
 }
--- app/Model/Like.php
+++ app/Model/Like.php
@@ -13,4 +13,13 @@ class Like extends AppModel {
         'foreignKey' => 'news_id'
     )
   );
+
+	public function afterSave($created, $options = array()) {
+		Cache::delete('news', 'data');
+	}
+
+	public function afterDelete($cascade = true) {
+		Cache::delete('news', 'data');
+	}
+	
 }
