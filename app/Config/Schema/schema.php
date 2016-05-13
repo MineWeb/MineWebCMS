55b30cd0e5a348d2cd591244849088b783f4d6e7
Valentin <contact@eywek.fr>
2016-05-13 18:12:53 +0200

    Modification du schema

--- app/Config/Schema/schema.php
+++ app/Config/Schema/schema.php
@@ -70,7 +70,7 @@ class AppSchema extends CakeSchema {
 				'server_cache' => 0,
 				'server_secretkey' => '',
 				'server_timeout' => 1,
-				'version' => '1.0.3',
+				'version' => '1.0.4',
 				'skype' => 'http://mineweb.org',
 				'youtube' => 'http://mineweb.org',
 				'twitter' => 'http://mineweb.org',
