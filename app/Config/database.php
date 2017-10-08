58fefc03f278d6d6afadaf683e80eae70e11fa52
Eywek <contact@eywek.fr>
2017-10-08 19:03:55 +0200

    Add condition

--- app/Config/database.php
+++ app/Config/database.php
@@ -1,13 +1,14 @@
 <?php
-class DATABASE_CONFIG {
+          class DATABASE_CONFIG {
 
-	public $default = array(
-		'datasource' => 'Database/Mysql',
-		'persistent' => false,
-		'host' => 'localhost',
-		'login' => 'root',
-		'password' => 'root',
-		'database' => 'eycraft',
-		'encoding' => 'utf8',
-	);
-}
+          	public $default = array(
+          		'datasource' => 'Database/Mysql',
+          		'persistent' => false,
+          		'host' => 'localhost',
+          		'login' => 'root',
+          		'password' => 'root',
+          		'database' => 'mineweb',
+          		'encoding' => 'utf8',
+          	);
+          }
+          
