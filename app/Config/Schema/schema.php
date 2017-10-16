1043b66acad335f525dd79316f040f0511ed0897
Eywek <contact@eywek.fr>
2017-10-16 15:55:28 +0200

    Fix version

--- app/Config/Schema/schema.php
+++ app/Config/Schema/schema.php
@@ -70,7 +70,7 @@ class AppSchema extends CakeSchema {
 				'server_cache' => 0,
 				'server_secretkey' => '',
 				'server_timeout' => 1,
-				'version' => '1.3.4',
+				'version' => '1.3.6',
 				'skype' => 'http://mineweb.org',
 				'youtube' => 'http://mineweb.org',
 				'twitter' => 'http://mineweb.org',
