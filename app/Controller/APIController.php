b97f419eae3bc5310787ca0b41943306b1578655
Jimmy Rich <jimmy.rich@outlook.com>
2018-11-13 17:41:06 +0100

    Edit Skin
    Update and remove outdated lignes

--- app/Controller/APIController.php
+++ app/Controller/APIController.php
@@ -43,15 +43,13 @@ class APIController extends AppController {
 	}
 
 	public function get_skin($name) {
-		header('Content-type: image/png');
-		$this->response->type('image/png');
+		header('Content-Type: image/png');
 		$this->autoRender = false;
 		echo $this->API->get_skin($name);
 	}
 
 	public function get_head_skin($name, $size = 50) {
-		header('Content-type: image/png');
-		$this->response->type('image/png');
+		header('Content-Type: image/png');
 		$this->autoRender = false;
 		echo $this->API->get_head_skin($name, $size);
 	}
