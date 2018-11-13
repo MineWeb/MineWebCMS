7c01f143623a9ed3c515c60e61a892652c8fc60b
Jimmy Rich <jimmy.rich@outlook.com>
2018-11-13 14:44:58 +0100

    improv: fix skin display (headers)

--- app/Controller/APIController.php
+++ app/Controller/APIController.php
@@ -43,13 +43,13 @@ class APIController extends AppController {
 	}
 
 	public function get_skin($name) {
-		$this->response->type('image/png');
+		header('Content-Type: image/png');
 		$this->autoRender = false;
 		echo $this->API->get_skin($name);
 	}
 
 	public function get_head_skin($name, $size = 50) {
-		$this->response->type('image/png');
+		header('Content-Type: image/png');
 		$this->autoRender = false;
 		echo $this->API->get_head_skin($name, $size);
 	}
