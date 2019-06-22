fa4256e2611135d58a1a4fe8c1fbf3ff084c3020
nivcoo <36514752+nivcoo@users.noreply.github.com>
2019-06-22 23:21:18 +0200

    improv. get real ip cloudflare

--- app/Controller/Component/StatisticsComponent.php
+++ app/Controller/Component/StatisticsComponent.php
@@ -1,71 +1,83 @@
 <?php
 
 /**
-* Composant des statistiques
-* @author Eywek
-* Avec l'aide de : http://openclassrooms.com/courses/des-statistiques-pour-votre-site
-**/
+ * Composant des statistiques
+ * @author Eywek
+ * Avec l'aide de : http://openclassrooms.com/courses/des-statistiques-pour-votre-site
+ **/
 
 /**
-* BDD
-*
-* -- Table visits --
-*
-* ip
-* created
-* referer
-* location
-*
-* -- Table rush_hours --
-*
-* created
-* visits
-*
-* -- connected --
-*
-* ip
-* created
-* location
-*
-**/
+ * BDD
+ *
+ * -- Table visits --
+ *
+ * ip
+ * created
+ * referer
+ * location
+ *
+ * -- Table rush_hours --
+ *
+ * created
+ * visits
+ *
+ * -- connected --
+ *
+ * ip
+ * created
+ * location
+ *
+ **/
 App::uses('AppHelper', 'View/Helper');
 App::uses('CakeSession', 'Model/Datasource');
 App::uses('CakeObject', 'Core');
 
-class StatisticsComponent extends CakeObject {
+class StatisticsComponent extends CakeObject
+{
 
-  function shutdown($controller) {
-  }
+    private $controller;
 
-  function beforeRender($controller) {
-  }
-
-  function beforeRedirect() {
-  }
-
-  function initialize($controller) {
-  }
-
-  function startup($controller) {
-    $cookie = CakeSession::read('visit_check');
-    if(!isset($cookie) OR empty($cookie)) {
-      $this->Visit = ClassRegistry::init('Visit');
-      $visits = $this->Visit->find('all', array('conditions' => array('ip' => $_SERVER["REMOTE_ADDR"], 'created LIKE' => date('Y-m-d').'%')));
-      if(empty($visits)) {
-        if(!empty($_SERVER['HTTP_REFERER'])) {
-          $referer = $_SERVER['HTTP_REFERER'];
-        } else {
-          $referer = 'null';
-        }
-        $user_agent = (isset($_SERVER['HTTP_USER_AGENT'])) ? $_SERVER['HTTP_USER_AGENT'] : 'null';
-        $language = (isset($_SERVER['HTTP_ACCEPT_LANGUAGE'])) ? $_SERVER['HTTP_ACCEPT_LANGUAGE'] : 'null';
-        $language = $language{0}.$language{1};
-        $this->Visit->read(null, null);
-        $this->Visit->set(array('ip' => $_SERVER["REMOTE_ADDR"], 'referer' => $referer, 'lang' => $language, 'navigator' => $user_agent, 'page' => "http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']));
-        $this->Visit->save();
-      }
-      CakeSession::write('visit_check', true, true, '1 day');
+    function shutdown($controller)
+    {
     }
-  }
+
+    function beforeRender($controller)
+    {
+    }
+
+    function beforeRedirect()
+    {
+    }
+
+    function initialize($controller)
+    {
+        $this->controller = $controller;
+    }
+
+    function startup($controller)
+    {
+        $cookie = CakeSession::read('visit_check');
+        if (!isset($cookie) OR empty($cookie)) {
+            $this->Visit = ClassRegistry::init('Visit');
+            $this->Util = $this->controller->Util;
+            $ip = $this->Util->getIP();
+            $visits = $this->Visit->find('all', array('conditions' => array('ip' => $ip, 'created LIKE' => date('Y-m-d') . '%')));
+            if (empty($visits)) {
+                if (!empty($_SERVER['HTTP_REFERER'])) {
+                    $referer = $_SERVER['HTTP_REFERER'];
+                } else {
+                    $referer = 'null';
+                }
+                $user_agent = (isset($_SERVER['HTTP_USER_AGENT'])) ? $_SERVER['HTTP_USER_AGENT'] : 'null';
+                $language = (isset($_SERVER['HTTP_ACCEPT_LANGUAGE'])) ? $_SERVER['HTTP_ACCEPT_LANGUAGE'] : 'null';
+
+                $language = $language{0} . $language{1};
+                $this->Visit->read(null, null);
+                $this->Visit->set(array('ip' => $ip, 'referer' => $referer, 'lang' => $language, 'navigator' => $user_agent, 'page' => "http://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']));
+                $this->Visit->save();
+            }
+            CakeSession::write('visit_check', true, true, '1 day');
+        }
+    }
 
 }
