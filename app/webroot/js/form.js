f4532085e181cee20225b06dae15dac090c6c490
Skkay <skkay.thomas@gmail.com>
2019-10-06 20:14:26 +0200

    Suppression de l'affichage du login et password dans la console

--- app/webroot/js/form.js
+++ app/webroot/js/form.js
@@ -99,7 +99,7 @@ function initForms() {
       }
     }
 
-    console.log(inputs);
+    //console.log(inputs);
 
     if(form.attr('data-checkData') !== undefined && form.attr('data-checkData').length > 0) {
       var check = window[form.attr('data-checkData')](inputs);
