0594e75e0b6f9dc461cfcb6f51a3d6f26fd92264
nivcoo <36514752+nivcoo@users.noreply.github.com>
2019-10-27 19:32:38 +0100

    improv. modify.php fix error

--- modify.php
+++ modify.php
@@ -1,12 +1,40 @@
 <?php
-
 $db = ConnectionManager::getDataSource('default');
-
-// 1.8.0
-@$db->query('ALTER TABLE `users` ADD COLUMN IF NOT EXISTS `uuid` VARCHAR(255) NOT NULL;');
-// end 1.8.0
-
 // for all update
+function add_column($table, $name, $sql) {
+  //global $db;
+  $db = ConnectionManager::getDataSource('default');
+  $verif = $db->query('SHOW COLUMNS FROM '.$table.';');
+  $execute = true;
+  foreach ($verif as $k => $v) {
+    if($v['COLUMNS']['Field'] == $name) {
+      $execute = false;
+      break;
+    }
+  }
+  if($execute) {
+    @$query = $db->query('ALTER TABLE `'.$table.'` ADD `'.$name.'` '.$sql.';');
+  }
+	
+}
+function remove_column($table, $name) {
+  //global $db;
+	$db = ConnectionManager::getDataSource('default');
+  $verif = $db->query('SHOW COLUMNS FROM '.$table.';');
+  $execute = false;
+  foreach ($verif as $k => $v) {
+    if($v['COLUMNS']['Field'] == $name) {
+      $execute = true;
+      break;
+    }
+  }
+  if($execute) {
+    @$query = $db->query('ALTER TABLE `'.$table.'` DROP COLUMN `'.$name.'`;');
+  }
+}
+// 1.8.0
+add_column('users', 'uuid', 'varchar(255) DEFAULT NULL');
+// end 1.8.0
 @clearFolder(ROOT.'/app/tmp/cache/models/');
 @clearFolder(ROOT.'/app/tmp/cache/persistent/');
 @unlink(ROOT.DS.'lang'.DS.'fr.json');
