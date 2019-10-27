7627dbd286e35116b4a3c92e4ad428ffa39bf7cc
Eywek <contact@eywek.fr>
2019-10-27 17:40:17 +0100

    chore: bump version to 1.8.0

---
+++ publish.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [ -z "$1" ]
+then
+   echo "You need to provide a version";
+   exit 1
+fi
+if [ -z "$2" ]
+then
+   echo "You need to provide a github token";
+   exit 1
+fi
+
+# git pull
+# git checkout development
+# git pull
+# git checkout master
+# NUM_COMMITS=`git log --oneline master ^development | wc -l | bc`
+# git rebase development
+# git rebase -i HEAD~$NUM_COMMITS
+echo $1 > VERSION
+git add .
+git commit -m "chore: bump version to $1"
+git tag v$1
+git push
+git push --tags
+gren release --limit 1 --data-source commits --token=$2
