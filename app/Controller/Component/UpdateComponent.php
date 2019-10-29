3b2607d627d9372ac4f7443bef6e971c3b9816f0
nivcoo <nico64800@live.fr>
2019-10-29 14:24:13 +0100

    improv. set limit time of update

--- app/Controller/Component/UpdateComponent.php
+++ app/Controller/Component/UpdateComponent.php
@@ -114,6 +114,7 @@ class UpdateComponent extends CakeObject
    * Update CMS files
    */
   public function updateCMS($componentUpdated = false) {
+      set_time_limit(200);
     if (!$componentUpdated) {
       // Here, this is the first step of the update. We're trying to keep this component
       // updated for update (in case of we need new behavior).
