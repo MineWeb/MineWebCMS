0e8caf049deb6aec0ee3944b89fb4f8274acd82a
Eywek <contact@eywek.fr>
2020-05-08 21:38:05 +0200

    fix: Add some htmlentities()

--- app/Controller/Component/StatisticsComponent.php
+++ app/Controller/Component/StatisticsComponent.php
@@ -64,16 +64,16 @@ class StatisticsComponent extends CakeObject
             $visits = $this->Visit->find('all', array('conditions' => array('ip' => $ip, 'created LIKE' => date('Y-m-d') . '%')));
             if (empty($visits)) {
                 if (!empty($_SERVER['HTTP_REFERER'])) {
-                    $referer = $_SERVER['HTTP_REFERER'];
+                    $referer = htmlentities($_SERVER['HTTP_REFERER']);
                 } else {
                     $referer = 'null';
                 }
-                $user_agent = (isset($_SERVER['HTTP_USER_AGENT'])) ? $_SERVER['HTTP_USER_AGENT'] : 'null';
-                $language = (isset($_SERVER['HTTP_ACCEPT_LANGUAGE'])) ? $_SERVER['HTTP_ACCEPT_LANGUAGE'] : 'null';
+                $user_agent = (isset($_SERVER['HTTP_USER_AGENT'])) ? htmlentities($_SERVER['HTTP_USER_AGENT']) : 'null';
+                $language = (isset($_SERVER['HTTP_ACCEPT_LANGUAGE'])) ? htmlentities($_SERVER['HTTP_ACCEPT_LANGUAGE']) : 'null';
 
                 $language = $language{0} . $language{1};
                 $this->Visit->read(null, null);
-                $this->Visit->set(array('ip' => $ip, 'referer' => $referer, 'lang' => $language, 'navigator' => $user_agent, 'page' => "http://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']));
+                $this->Visit->set(array('ip' => $ip, 'referer' => $referer, 'lang' => $language, 'navigator' => $user_agent, 'page' => "http://" . htmlentities($_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'])));
                 $this->Visit->save();
             }
             CakeSession::write('visit_check', true, true, '1 day');
