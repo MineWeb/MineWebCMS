811207605d4b6f2708d9396c8fac2288fa1fadd5
nivcoo <nico64800@live.fr>
2021-01-04 10:16:43 +0100

    improv. reformat all code

--- app/Config/Schema/i18n.php
+++ app/Config/Schema/i18n.php
@@ -25,27 +25,25 @@
  *
  * cake schema run create i18n
  */
-class I18nSchema extends CakeSchema
-{
+class I18nSchema extends CakeSchema {
 
-    public $name = 'i18n';
-    public $i18n = [
-        'id' => ['type' => 'integer', 'null' => false, 'default' => null, 'length' => 10, 'key' => 'primary'],
-        'locale' => ['type' => 'string', 'null' => false, 'length' => 6, 'key' => 'index'],
-        'model' => ['type' => 'string', 'null' => false, 'key' => 'index'],
-        'foreign_key' => ['type' => 'integer', 'null' => false, 'length' => 10, 'key' => 'index'],
-        'field' => ['type' => 'string', 'null' => false, 'key' => 'index'],
-        'content' => ['type' => 'text', 'null' => true, 'default' => null],
-        'indexes' => ['PRIMARY' => ['column' => 'id', 'unique' => 1], 'locale' => ['column' => 'locale', 'unique' => 0], 'model' => ['column' => 'model', 'unique' => 0], 'row_id' => ['column' => 'foreign_key', 'unique' => 0], 'field' => ['column' => 'field', 'unique' => 0]]
-    ];
+	public $name = 'i18n';
 
-    public function before($event = [])
-    {
-        return true;
-    }
+	public function before($event = array()) {
+		return true;
+	}
 
-    public function after($event = [])
-    {
-    }
+	public function after($event = array()) {
+	}
+
+	public $i18n = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'length' => 10, 'key' => 'primary'),
+		'locale' => array('type' => 'string', 'null' => false, 'length' => 6, 'key' => 'index'),
+		'model' => array('type' => 'string', 'null' => false, 'key' => 'index'),
+		'foreign_key' => array('type' => 'integer', 'null' => false, 'length' => 10, 'key' => 'index'),
+		'field' => array('type' => 'string', 'null' => false, 'key' => 'index'),
+		'content' => array('type' => 'text', 'null' => true, 'default' => null),
+		'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1), 'locale' => array('column' => 'locale', 'unique' => 0), 'model' => array('column' => 'model', 'unique' => 0), 'row_id' => array('column' => 'foreign_key', 'unique' => 0), 'field' => array('column' => 'field', 'unique' => 0))
+	);
 
 }
